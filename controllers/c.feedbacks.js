71d85bfa9302e29fa76d9e8bf3aab3f6d84b27ca
Amar Panjwani <amar.panjwani@gmail.com>
2021-10-08 01:54:51 -0700

    clean up controllers code

--- controllers/c.feedbacks.js
+++ controllers/c.feedbacks.js
@@ -5,15 +5,12 @@ module.exports = {
     create
 }
 
-function create(req, res){
-    console.log(req.body)
-
-    Project.findById(req.params.id, function(err, projectDocument){
-        console.log(projectDocument, "<--projectDoc")
+function create(req, res) {
+    Project.findById(req.params.id, function (err, projectDocument) {
         req.body.userId = req.user._id;
         req.body.userName = req.user.name;
         projectDocument.feedbacks.push(req.body)
-        projectDocument.save(function(err){
+        projectDocument.save(function (err) {
             res.redirect(`/projects/${req.params.id}`)
         });
     });
--- controllers/c.projects.js
+++ controllers/c.projects.js
@@ -14,12 +14,9 @@ module.exports = {
     allProjects
 }
 
-function edit(req,res){
-    console.log(req.params, "<--req.params")
-    Project.findById(req.params.id, function(err, foundProject){
-        console.log(foundProject);
+function edit(req, res) {
+    Project.findById(req.params.id, function (err, foundProject) {
         if (!foundProject.user.equals(req.user._id)) return res.redirect('/projects');
-        console.log(foundProject, "<--LOOK AT ID")
         res.render('projects/v.p.edit.ejs', {
             project: foundProject
         })
@@ -27,146 +24,63 @@ function edit(req,res){
 
 }
 
-function update(req, res){
-    Project.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, updatedProject){
-     console.log(updatedProject)
-        res.redirect('/projects')
+function update(req, res) {
+    Project.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, updatedProject) {
+        res.redirect(`/projects/${updatedProject._id}`)
     })
-  }
+}
 
-// function update(req,res){
-//     // Project.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, foundProject){
-//     //     console.log(foundProject);
-//     Project.findByIdAndUpdate(req.params.id, req.body, {new: true}, {
-//         res.redirect('/projects');
-//     })
-
-// }
-
-async function update2(req,res){
-
-    try{
-      const updatedProjectDocument = Project.findByIdAndUpdate(req.params.id, req.body, {new: true})
-      console.log(updatedProjectDocument, '<--updatedProjectDocument')
-      res.redirect('/projects')
-  
-    }catch(err){
-      console.log(err)
-      res.send('There was an error in the update function', err)
-    }
-  }
-
-// function update(req, res){
-//     Project.create(req.params.id, req.body, function(err, updatedProject){
-//         console.log(updatedProject, '<-- updatedProject')
-//         res.redirect('/projects');
-//     });
-// }
-
-function deleteProject(req, res){
-    Project.deleteOne({_id: req.params.id}, function(err, deletedProject){
-        if (err) { console.log(err, '<--- error')}
-        console.log(deletedProject);
+function deleteProject(req, res) {
+    Project.deleteOne({ _id: req.params.id }, function (err, deletedProject) {
+        if (err) { console.log(err) }
         res.redirect('/projects/all');
     });
 }
 
-function deleteOne(id){
+function deleteOne(id) {
     const idx = projects.findIndex(project => project.id === parseInt(id));
     projects.splice(idx, 1);
 }
 
-function show(req, res){
-    Project.findById(req.params.id, function(err, foundProject){
-        console.log(foundProject);
+function show(req, res) {
+    Project.findById(req.params.id, function (err, foundProject) {
         res.render('projects/v.p.show.ejs', {
             project: foundProject
         })
     })
 }
 
-function allProjects(req, res){
-    console.log(req.body, "<--req.body")
-    Project.find({}, function(err, projectsDocuments){
+function allProjects(req, res) {
+    Project.find({}, function (err, projectsDocuments) {
         res.render('projects/v.p.allProjects.ejs', {
             projects: projectsDocuments
         })
     })
 }
 
-function index(req, res){
-    console.log(req.user,"<--req.user")
-    console.log(req.body.user,"<--req.body.user")
-    console.log(req.params,"<--req.params")
-    Project.find({user: req.user._id}, function(err, projectsDocuments){
+function index(req, res) {
+    Project.find({ user: req.user._id }, function (err, projectsDocuments) {
         res.render('projects/v.p.index.ejs', {
             projects: projectsDocuments
         })
     })
 }
 
-// 
-
 function newProject(req, res) {
     res.render('projects/v.p.new.ejs')
 }
 
 function create(req, res) {
-    console.log(req.body)
-    //req.body.age = parseInt(req.body.age)
     Project.create(req.body, function (err, createdProject) {
         if (err) {
             console.log(err);
             return res.redirect('/new')
         }
-        console.log(createdProject, "<---FIRST")
         createdProject.user = req.user._id
         createdProject.userName = req.user.name
-        console.log(createdProject, "<---SECOND")
-
-        createdProject.save(function(err){
-            console.log(createdProject, "<---THIRD")
+        createdProject.save(function (err) {
             res.redirect(`/projects/${createdProject._id}`)
         })
-        
+
     })
 }
-
-// function index(req, res) {
-//     Profile.find({}, function (err, profilesDocuments) {
-//         console.log(profilesDocuments, ' <-- movie documents')
-//         res.render('profiles/v.p.index', {
-//             profiles: profilesDocuments
-//         })
-//     })
-// }
-
-// above functions have beenn reformatted
-// for Mongoose and MongoDB
-
-// function update(req, res) {
-//     // The following will also do the job
-//     // req.body.done = !!req.body.done;
-//     Profile.update(req.params.id, req.body);
-//     res.redirect('/profiles');
-// }
-
-// function edit(req, res) {
-//     res.render('profiles/v.p.edit.ejs', {
-//         profile: Profile.getOne(req.params.id)
-//     });
-// }
-
-// function show(req, res) {
-//     res.render('profiles/v.p.show.ejs', {
-//         profile: Profile.getOne(req.params.id)
-//     })
-// }
-
-
-
-// function deleteProfile(req, res) {
-//     Profile.deleteOne(req.params.id)
-//     res.redirect('/profiles');
-// }
-
