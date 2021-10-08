a9bab62e06d1fd0877a41fae88d809e77c6ed4d8
Amar Panjwani <amar.panjwani@gmail.com>
2021-10-08 01:30:31 -0700

    cleaned up config files

--- config/database.js
+++ config/database.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 
-// replace your database connection string here
-// mongoose.connect('mongodb://localhost/profiles')
-mongoose.connect(process.env.DATABASE_URL ,{
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
 
-// shortcut to moongoose.connect obj
 const db = mongoose.connection;
 
-// database connection event
 db.on('connected', function () {
   console.log(`Mongoose connected to: ${db.host}:${db.port}`);
 });
--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require("../models/m.user")
-//Require your User Model here!
 
-// configuring Passport!
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK
 },
   function (accessToken, refreshToken, profile, cb) {
-    // a user has logged in via OAuth!
-    // refer to the lesson plan from earlier today in order to set this up
-    // console.log(profile, "<----- Profile"); // <--- Is going to be the users that just logged information from google
-    // do we put googleId in single quotes or not?
     User.findOne({ googleId: profile.id }, function (err, userDoc) {
       if (err) return cb(err);
 
@@ -31,20 +25,6 @@ passport.use(new GoogleStrategy({
           if (err) return cb(err);
           return cb(null, newUser);
         })
-
-        // The above is equivelant to
-        // Student.create(
-        //   {
-        //     name: profile.displayName,
-        //     email: profile.emails[0].value, // THis object should match the keys and values in our schema, be sure to include the
-        //     // googleId
-        //     googleId: profile.id,
-        //   },
-        //   function (err, newStudentDoc) {
-        //     if (err) return cb(err);
-        //     return cb(null, newStudent);
-        //   }
-        // );
       }
     })
   }
@@ -55,11 +35,7 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(function (id, done) {
-
-  // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
-  // When you call this done function passport assigns the user document to req.user, which will 
-  // be availible in every Single controller function, so you always know the logged in user
-  User.findById(id, function (err, userDoc){
+  User.findById(id, function (err, userDoc) {
     done(err, userDoc)
   });
 });
